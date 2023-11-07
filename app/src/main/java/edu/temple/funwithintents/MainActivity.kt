35f0f4c50de2d7eac9f84821b59c385c71f84817
Powerboy3551 <145390119+Powerboy3551@users.noreply.github.com>
2023-11-07 15:05:47 -0500

    Implemented feature in order for the activity to be shared

--- app/src/main/java/edu/temple/funwithintents/MainActivity.kt
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -1,6 +1,7 @@
 package edu.temple.funwithintents
 
 import android.os.Bundle
+import android.content.Intent
 import android.widget.EditText
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
@@ -15,7 +16,18 @@ class MainActivity : AppCompatActivity() {
 
         // When the user clicks this button, share the text
         findViewById<ImageButton>(R.id.shareImageButton).setOnClickListener {
+            // Get the text from the EditText
+            val textToShare = editText.text.toString()
 
+            // Create an Intent to share text
+            val sendIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, textToShare)
+            }
+
+            //Share the text
+            startActivity(Intent.createChooser(sendIntent, "Share via"))
         }
     }
 }
